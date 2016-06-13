e7c8ad29a996c2717d030f3ed44a3fe54776b01f
Carmelo Piccione <carmelo.piccione@gmail.com>
2016-06-12 20:11:01 -0400

    coh pofable map improvements

--- src/coh_pofable_map.ml
+++ src/coh_pofable_map.ml
@@ -1,13 +1,23 @@
 open Coh_primitives
 
+module type S =
+sig
+  module Key : Pofable.S
+  module Value : Pofable.S
+  include Coh_map.S with module Key := Key and module Value := Value
+end
 
-module Make(Key:Pofable.S)(Value:Pofable.S) =
+module Make(Key:Pofable.S)(Value:Pofable.S) 
+  : S with module Key = Key and module Value = Value =
 struct
 module type S =
 sig
-  include Coh_map.S with module Key = Key and module Value = Value
+  include S with module Key = Key and module Value = Value
 end
 
-
+module Parent = Coh_map.Make(Key)(Value)
+module Key = Key
+module Value = Value
+include (Parent : S with module Key := Key and module Value := Value)
 end
 
